c1474ede6a24b4f70b226ef22371f5fb7587f39f
Gungun Gupta <104621253+GungunGupta14@users.noreply.github.com>
2023-02-21 20:44:29 +0530

    Added solution - LeetHub

---
+++ Pattern 8 - GFG/pattern-8.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	    // code here
+	    for(int i=n;i>=1;i--){
+            for(int j=n-i;j>0;j--){
+                cout<<" ";
+            }
+            for(int k=1;k<2*i;k++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
