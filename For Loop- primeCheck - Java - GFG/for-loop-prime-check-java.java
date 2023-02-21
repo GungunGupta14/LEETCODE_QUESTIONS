9a35c34196d74891ef3ebdb9d383220b1cbe4644
Gungun Gupta <104621253+GungunGupta14@users.noreply.github.com>
2023-02-21 19:24:23 +0530

    Added solution - LeetHub

---
+++ For Loop- primeCheck - Java - GFG/for-loop-prime-check-java.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+
+        // taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // take testcases
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            // taking the number
+            int n = sc.nextInt();
+            Geeks obj = new Geeks();
+
+            // caling isPrime method
+            // with n as argument
+            obj.isPrime(n);
+        }
+    }
+}
+
+// Position this line where user code will be pasted.
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Geeks {
+
+    static void isPrime(int n) {
+
+        if(n<=1){
+
+            System.out.println("No");
+
+        }
+
+        int count =0;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+
+            // Your code here
+
+            if(n%i==0){
+
+               count++;
+
+            }
+
+ }
+
+        if(n>1){
+
+            if(count>=1){
+
+                System.out.println("No");
+
+            }
+
+            else{
+
+             System.out.println("Yes");   
+
+            }
+
+          }
+
+         }
+
+};
