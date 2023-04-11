56c82eba1d9c3ff2812cdc153181bd80a59ad4c4
Gungun Gupta <104621253+GungunGupta14@users.noreply.github.com>
2023-04-11 20:57:07 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (37.91%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+   double myPow(double x, long long n) {
+        if(n == 0) 
+            return 1;
+        if(n<0) 
+            return myPow(1/x, (-1)*n);
+        double temp = myPow(x, n/2);
+        if(n%2 == 0)
+            return temp*temp;
+        return temp*temp*x;
+    }
+};
