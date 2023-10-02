8a1efbc7d068cf03bb98417c12a94af045b8b42f
Gungun Gupta <104621253+GungunGupta14@users.noreply.github.com>
2023-10-02 16:58:21 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (10.53%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int hash[26]={0};
+        for(int i=0;i<s.size();i++){
+            hash[s[i]-'a']++;
+        }
+        char freqChar;
+        int maxFreq=INT_MIN;
+        for(int i=0;i<26;i++){
+            if(hash[i]>maxFreq){
+                maxFreq=hash[i];
+                freqChar=i+'a';
+            }
+        }
+        int index=0;
+        while(maxFreq>0 && index<s.size()){
+            s[index]=freqChar;
+            maxFreq--;
+            index+=2;
+        }
+        if(maxFreq!=0){
+            return "";
+        }
+        hash[freqChar-'a']=0;
+        for(int i=0;i<26;i++){
+            while(hash[i]>0){
+                index=index>=s.size()?1:index;
+                s[index]=i+'a';
+                hash[i]--;
+                index+=2;
+            }
+        }
+        return s;
+        
+    }
+};
