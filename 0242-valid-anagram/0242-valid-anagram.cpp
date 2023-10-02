06696dd651cf2d78116e6f5ae2b360c91599d924
Gungun Gupta <104621253+GungunGupta14@users.noreply.github.com>
2023-10-02 14:28:43 +0530

    Time: 7 ms (77.96%), Space: 7.8 MB (13.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
+        int freqtable[256]={0};
+        for(int i=0;i<s.size();i++){
+            freqtable[s[i]]++;
+            
         }
-        return false;
+        for(int i=0;i<t.size();i++){
+            freqtable[t[i]]--;
+        }
+        for(int i=0;i<256;i++){
+            if(freqtable[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
