b142cb59abc66bfcb442e78289ac8ca840ae9610
Gungun Gupta <104621253+GungunGupta14@users.noreply.github.com>
2023-10-12 22:56:15 +0530

    Time: 2 ms (77.46%), Space: 6.9 MB (63.63%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    string addRE(string num1,int p1, string num2,int p2, int carry=0){
+    void addRE(string&num1,int p1, string&num2,int p2,int carry,string &ans){
         if(p1<0 &&p2<0){
             if(carry!=0){
-                return string(1,carry+'0');
+                ans.push_back(carry+'0');
             }
-            return "";
+            return ;
         }
         
         int n1=(p1>=0? num1[p1]:'0')-'0';
@@ -13,13 +13,13 @@ public:
         int sum=n1+n2+carry;
         int digit=sum%10;
         carry=sum/10;
-        string ans="";
         ans.push_back(digit+'0');
-        ans+=addRE(num1,p1-1,num2,p2-1,carry);
-        return ans;
+        addRE(num1,p1-1,num2,p2-1,carry,ans);
+      
     }
     string addStrings(string num1, string num2) {
-        string ans=addRE(num1,num1.size()-1,num2,num2.size()-1);
+        string ans="";
+        addRE(num1,num1.size()-1,num2,num2.size()-1,0,ans);
         reverse(ans.begin(),ans.end());
         return ans;
     }
