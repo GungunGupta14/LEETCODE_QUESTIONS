ec4fbc01280dcdec1e02006765f75807b051c9c5
Gungun Gupta <104621253+GungunGupta14@users.noreply.github.com>
2023-12-03 14:20:48 +0530

    Time: 8 ms (5.86%), Space: 10.5 MB (6.16%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        int pos=s.find(part);
-        while(pos!=string::npos){
-            s.erase(pos,part.length());
-            pos=s.find(part);
+    void removee(string &s, string& part){
+        int found=s.find(part);
+        if(found!=string::npos){
+            string left_part=s.substr(0,found);
+            string right_part=s.substr(found+part.size(),s.size());
+            s=left_part+right_part;
+            removee(s,part);
         }
+        else{
+            return ;
+        }
+        
+    }
+    string removeOccurrences(string s, string part) {
+        removee(s,part);
         return s;
     }
 };
